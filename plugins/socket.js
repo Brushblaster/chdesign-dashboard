de5125792d9a5af9d8ce7dc1b195912cd3ee7f2b
Brushblaster <33086404+Brushblaster@users.noreply.github.com>
2018-10-02 07:51:45 +0200

    Update socket.js

--- plugins/socket.js
+++ plugins/socket.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueSocketIo from 'vue-socket.io'
 import io from 'socket.io-client'
 
-let prodHost = `https://dev.ch-design.ch`
+let prodHost = `https://${process.env.PROD_DOMAIN}`
 
 Vue.use(
   VueSocketIo,
