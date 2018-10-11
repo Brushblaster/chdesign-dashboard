e57acdb7e72ba90f8bef2396960979c655cf04e2
Kaech <denis.kaech@gmail.com>
2018-10-11 14:24:27 +0200

    updated database connection

--- db/index.js
+++ db/index.js
@@ -1,32 +1,36 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 const CustomerModel = require('./models/customers')
+const CoursesModel = require('./models/courses')
 
 const options = {
   useNewUrlParser: true,
   socketTimeoutMS: 0,
   keepAlive: true,
-  reconnectTries: 30
+  reconnectTries: 30,
+  user: process.env.MONGODB_USER,
+  pass: process.env.MONGODB_PASS
 }
 
-let buildUpUri =
+/* let buildUpUri =
   'mongodb://' +
   process.env.MONGODB_USER +
   ':' +
   process.env.MONGODB_PASS +
   '@' +
   process.env.MONGODB_ADDRESS_PROD +
-  '/chdesign?authSource=admin'
+  '/chdesign?authSource=admin' */
 
 let uri = buildUpUri
 
 mongoose
   .connect(
-    uri,
+    `mongodb://${process.env.MONGODB_ADDRESS_PROD}:27017/chdesign?authSource=admin`,
     options
   )
   .catch(error => console.log('uri error: ' + uri, error))
 
 module.exports = {
-  CustomerDB: CustomerModel
+  CustomerDB: CustomerModel,
+  CoursesDB: CoursesModel
 }
